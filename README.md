0edb4ea950c556ca27dface777cb6bd770e776fc
Tobyglei <tobyglei@gmail.com>
2020-10-31 15:22:22 +0800

    新增streamlit 示例

--- README.md
+++ README.md
@@ -8,13 +8,16 @@
 
 1. Fork [Streamlit](https://github.com/tobyglei/hello-streamlit)
 2. 在21云盒子上创建一个 **云服务**， 并允许21云盒子访问你的代码库
-3. 用 **`Python 3.7`** 环境
-4. 启动指命 **`streamlit run first_app.py --server.port=10000`**
+3. 选上 **`Python 3`** 环境
+4. 启动指命:  **`streamlit run first_app.py --server.enableCORS=false --server.port=10000 --browser.serverAddress='0.0.0.0' --server.headless=true `**
 5. 点"创建"
 
+<img src="screenshot.png">
 
+部署成功后的截图
 
+<img src="https://21box-assets.oss-cn-beijing.aliyuncs.com/deployed_result.png" width="300px">
 
-就是这么容易!当构建完成，你的应用将会在21云盒子的子域名能访问。
+当构建完成，你的应用将会在21云盒子的子域名能访问。
 
 其他可以部署在21云盒子的示例，请看 [技术文档](https://www.21yunbox.com/docs/v2/)。
--- first_app.py
+++ first_app.py
@@ -1,9 +1,42 @@
+# Example app from
+# https://docs.streamlit.io/en/stable/tutorial/create_a_data_explorer_app.html#let-s-put-it-all-together
+
 import streamlit as st
-# To make things easier later, we're also importing numpy and pandas for
-# working with sample data.
 import numpy as np
 import pandas as pd
 
-print(st.__version__)
-print(np.__version__)
-print(pd.__version__)
+st.title('Streamlit Example on 21YunBox -- Uber pickups in NY')
+
+DATE_COLUMN = 'date/time'
+# 原文件在 s3上
+# DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+#             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = 'https://21box-assets.oss-cn-beijing.aliyuncs.com/uber-raw-data-sep14.csv.gz'
+
+@st.cache
+def load_data(nrows):
+    with open(filename, 'rb') as csvfile:
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
