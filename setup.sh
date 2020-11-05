f3a4fb0904bc8bd89fd52bb7b5e147a5d7b819a1
Tobyglei <tobyglei@gmail.com>
2020-11-05 21:40:11 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"21yunbox@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS = false\n\
+enableXsrfProtection = false\n\
+enableWebsocketCompression = false\n\
+port = 10000\n\
+" > ~/.streamlit/config.toml
